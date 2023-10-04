a0094b019777263fa2d643dd9e3525825051cb57
Usmanzaib356 <110809791+Usmanzaib356@users.noreply.github.com>
2023-10-04 13:28:12 -0700

    #

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import Header from '@/Components/Header'
-import '../Style/index.css'
 import '../Style/media.css'
+import '../Style/index.css'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import { Inter } from 'next/font/google'
